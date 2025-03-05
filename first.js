d96693b238914f759cbd06360ffaf86e5adb48c1
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-03-05 00:45:17 +0000

    factorial

--- first.js
+++ first.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+//A function to compute a factorial of a number
+
+function factorial(f){
+    if (f == 0){
+        return 1;
+    }else{
+        return factorial(f-1) * f;
+    }
+}
+
+console.log(factorial(5));
