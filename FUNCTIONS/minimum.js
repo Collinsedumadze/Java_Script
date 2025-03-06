7b44ed3061d8ce98797bdc077f823c158b1cf083
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-03-06 23:33:13 +0000

    minimum

---
+++ FUNCTIONS/minimum.js
@@ -0,0 +1,13 @@
+// Like the standard function Math.min that returns its smallest argument. Define the function min that takes two arguments and returns their minimum.
+
+function getMin(x , y){
+    let min = 0;
+    if(x > y){
+       return min += y; 
+    }else{
+       return min += x;
+    }
+
+}
+
+console.log(getMin(0, 4));
