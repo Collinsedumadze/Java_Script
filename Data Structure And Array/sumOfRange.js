079a9e89a7ab6717ac3af4467234f1a92a6e673a
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-03-15 08:41:15 +0000

    getting a function and range and sum

---
+++ Data Structure And Array/sumOfRange.js
@@ -0,0 +1,23 @@
+//console.log(sum(range(1, 10)));
+
+/* Write a range function that takes two arguments, start and end, and
+ returns an array containing all the numbers from start up to and in
+cluding end. */
+
+function range (start, end){
+    let rangeArray = [];
+    for (let i = start; i <= end; i++){
+        rangeArray.push(i);
+    }
+        return rangeArray;
+}
+
+console.log(range(1, 10));
+
+function sum(rangeArray){
+    let sum = 0;
+    for (let i = 0; i < rangeArray.length; i++){
+        sum += rangeArray[i];
+    }
+    return sum;
+} 
